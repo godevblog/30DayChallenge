593f8b9fa4d2612de0a4e75e25b017ce3b056fc0
Krzysztof Owsiany <gocom@o2.pl>
2018-02-07 20:39:27 +0100

    5th post about Rx. Interval

---
+++ Interval/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var subscribents = new List<IDisposable>();
+
+			var observableInterval = Observable.Interval(
+				TimeSpan.FromMilliseconds(200));
+
+			var observableIntervalSheduler = Observable.Interval(
+				TimeSpan.FromMilliseconds(2000), 
+				TaskPoolScheduler.Default);
+
+			AddSubscribent(subscribents, observableInterval);
+			AddSubscribent(subscribents, observableIntervalSheduler);
+
+			Console.ReadKey();
+
+			DisposeAllSubscribents(subscribents);
+		}
+
+		private static void DisposeAllSubscribents(List<IDisposable> subscribents)
+		{
+			subscribents.ForEach(subscribent => subscribent.Dispose());
+		}
+
+		private static void AddSubscribent(List<IDisposable> subscribents, IObservable<long> observable)
+		{
+			var subscribent = observable
+				.Subscribe(
+					index => { Console.WriteLine($"Index: {index}"); },
+					exception => { Console.WriteLine($"Exception: {exception}"); },
+					() => { Console.WriteLine("Completed"); });
+
+			subscribents.Add(subscribent);
+		}
+	}
+}
