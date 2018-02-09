41ca6be3333966b07860e474cdbc395675cf85a5
Krzysztof Owsiany <gocom@o2.pl>
2018-02-09 18:24:11 +0100

    7th example about Rx Delay for 30 day challenge

---
+++ Delay/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delay
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var rand = new Random();
+			var spies = new List<IDisposable>();
+			var observableInterval = Observable.Interval(TimeSpan.FromMilliseconds(5000));
+
+			var subscribeInterval = observableInterval.Subscribe(index =>
+			{
+				Console.WriteLine($"{DateTime.UtcNow} Followed!");
+			});
+			
+			for (var i = 0; i < 10; i++)
+			{
+				spies.Add(new Spy(observableInterval, rand.Next(3000), $"Spy {i}"));
+			}
+
+			Console.ReadKey();
+
+			subscribeInterval.Dispose();
+			spies.ForEach(spy => spy.Dispose());
+		}
+	}
+}
---
+++ Delay/Spy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Delay
+{
+	internal class Spy : IDisposable
+	{
+		private string _spyName;
+		private IDisposable _spySubscribe;
+
+		public Spy(IObservable<long> observableInterval, int time, string spyName)
+		{
+			_spyName = spyName;
+
+			var delay = TimeSpan.FromMilliseconds(time);
+			_spySubscribe = observableInterval.Delay(delay)
+				.Subscribe(
+				index =>
+				{
+					Console.WriteLine($"{DateTime.UtcNow} {_spyName}");
+				},
+				exception =>
+				{
+					Console.WriteLine(exception);
+				},
+				() =>
+				{
+					Console.WriteLine("Completed");
+				});
+		}
+
+		public void Dispose()
+		{
+			_spySubscribe?.Dispose();
+		}
+	}
+}
