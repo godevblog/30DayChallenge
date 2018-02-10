c180b3e10d613dcac3991abe33495f04f1989820
Krzysztof Owsiany <gocom@o2.pl>
2018-02-10 15:02:27 +0100

    8th example of Rx, Sample for 30 day challenge

---
+++ Sample/PickASample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Sample
+{
+	public class PickASample : IDisposable
+	{
+		private IDisposable _simpleObservable;
+		private int _seconds;
+
+		public PickASample(IObservable<long> observable, int seconds)
+		{
+			_seconds = seconds;
+
+			var timeSpan = TimeSpan.FromSeconds(seconds);
+
+			_simpleObservable = observable
+				.Sample(timeSpan)
+				.Timestamp()
+				.Subscribe(
+					timestamp =>
+					{
+						Console.WriteLine($"Sample every {_seconds} seconds: {timestamp}.");
+					},
+					exception =>
+					{
+						Console.WriteLine(exception);
+					},
+					() =>
+					{
+						Console.WriteLine("Completed");
+					});
+		}
+
+		public void Dispose()
+		{
+			_simpleObservable.Dispose();
+		}
+	}
+}
---
+++ Sample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var observable = Observable.Interval(TimeSpan.FromMilliseconds(10));
+
+			var sampleList = new List<IDisposable>
+			{
+				new PickASample(observable, 1),
+				new PickASample(observable, 3),
+				new PickASample(observable, 5)
+			};
+
+			Console.ReadKey();
+
+			sampleList.ForEach(s => s.Dispose());
+		}
+	}
+}
