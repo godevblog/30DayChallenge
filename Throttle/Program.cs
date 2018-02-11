feedc2b44e491a4332326c25a95b353343575f2e
Krzysztof Owsiany <gocom@o2.pl>
2018-02-11 14:05:28 +0100

    Add another Rx example for 30 day challege, now for Throttle

---
+++ Throttle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Throttle
+{
+	class Program
+	{
+		static void Main()
+		{
+			var streamPublisher = new StreamPublisher();
+
+			var throttle = streamPublisher.Stream.Throttle(TimeSpan.FromMilliseconds(500));
+
+			var throttleSubscribe = throttle.Timestamp().Subscribe(
+				x => Console.WriteLine("Last item {0}: {1}", x.Value, x.Timestamp),
+				Console.WriteLine,
+				() => Console.WriteLine("Throttle completed"));
+
+			Console.ReadKey();
+
+			throttleSubscribe.Dispose();
+			streamPublisher.Dispose();
+		}
+	}
+}
---
+++ Throttle/StreamPublisher.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading;
+
+namespace Throttle
+{
+	internal class StreamPublisher : IDisposable
+	{
+		private bool _end;
+		private bool _publish;
+		private IDisposable _switcherSubscribe;
+
+		public IObservable<int> Stream => GetStream().ToObservable();
+
+		public StreamPublisher()
+		{
+			_publish = true;
+			InitializeSwitcher();
+		}
+
+		private IEnumerable<int> GetStream()
+		{
+			var i = 0;
+
+			while (!_end)
+			{
+				while (!_publish)
+				{
+					Thread.Sleep(100);
+				}
+
+				Console.Write($"{i},");
+
+				yield return i++;
+
+				Thread.Sleep(100);
+			}
+		}
+
+		private void InitializeSwitcher()
+		{
+			var switcherTimeSpan = TimeSpan.FromSeconds(5);
+			var switcher = Observable.Interval(switcherTimeSpan);
+			_switcherSubscribe = switcher.Subscribe(x =>
+				{
+					_publish = !_publish;
+
+					var swithcTo = _publish ? "publish (unlock stream)" : "not publish (lock stream)";
+
+					Console.WriteLine();
+					Console.WriteLine($"Switch to {swithcTo}");
+					Console.WriteLine();
+				},
+				Console.WriteLine,
+				() => { Console.WriteLine("Switcher completed"); }
+			);
+		}
+
+		public void Dispose()
+		{
+			_end = true;
+			_switcherSubscribe.Dispose();
+		}
+	}
+}
