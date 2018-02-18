029468a4622026d36094ceab63d4a5b3647ba82c
Krzysztof Owsiany <gocom@o2.pl>
2018-02-18 14:30:46 +0100

    16th post code about Rx

---
+++ Concat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Concat
+{
+	class Program
+	{
+		static void Main()
+		{
+			var observableSequence = Observable.Range(0, 5);
+
+			for (var i = 0; i < 10; i++)
+			{
+				observableSequence = observableSequence.Concat(CreateGenerator(0, i * 10, 2, 500));
+				observableSequence = observableSequence.Concat(Observable.Range(0, 5));
+			}
+
+			observableSequence.Subscribe(Console.WriteLine);
+
+			Console.ReadKey();
+		}
+
+		private static IObservable<int> CreateGenerator(int initialState, int conditionValue, int incrementValue, int periodValue)
+		{
+			var period = TimeSpan.FromMilliseconds(periodValue);
+
+			var generator = Observable.Generate(
+				initialState,
+				condition => condition <= conditionValue,
+				iterate => iterate + incrementValue,
+				resultSelector => resultSelector,
+				timeSelector => period
+			);
+
+			return generator;
+		}
+	}
+}
