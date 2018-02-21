8c529ac7d971a3ca8d37c660d5b74e2d8dee7c22
Krzysztof Owsiany <gocom@o2.pl>
2018-02-21 21:11:38 +0100

    19th post about Rx for 30 day challenge

---
+++ Ambiguous/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Reactive.Linq;
+using System.Threading;
+using RXLib.Factories;
+
+namespace Ambiguous
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var observableGenerator1 = GeneratorFactory.CreateGenerator(0, 100, 1, 30);
+			var observableGenerator2 = GeneratorFactory.CreateGenerator(1000, 1050, 1, 20);
+			var observableGenerator3 = GeneratorFactory.CreateGenerator(10000, 10020, 1, 40);
+
+			var observableAmbiguous = Observable.Amb(observableGenerator1, observableGenerator2, observableGenerator3);
+
+			var subscribent = observableAmbiguous.Subscribe(
+				item =>
+				{
+					Console.WriteLine($"Amb: {item}");
+				},
+				exception => Console.WriteLine(exception));
+
+			Console.ReadKey();
+
+			subscribent.Dispose();
+		}
+	}
+}
---
+++ RXLib/Factories/GeneratorFactory.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Reactive.Linq;
+
+namespace RXLib.Factories
+{
+	public static class GeneratorFactory
+	{
+		public static IObservable<int> CreateGenerator(int initialState, int conditionValue, int incrementValue, int periodValue)
+		{
+			var period = TimeSpan.FromMilliseconds(periodValue);
+
+			var generator = Observable.Generate(
+				initialState,
+				condition => condition <= conditionValue,
+				iterate => iterate + incrementValue,
+				resultSelector => resultSelector,
+				timeSelector => period
+			);
+
+			return generator;
+		}
+	}
+}
