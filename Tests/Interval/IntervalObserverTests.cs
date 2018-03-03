0baca313c5db2503d9f72d3bfce29b5a2339a1e2
Krzysztof Owsiany <gocom@o2.pl>
2018-03-03 16:57:49 +0100

    29 example about Rx and testing for 30 day challenge

---
+++ Tests/Interval/IntervalObserverTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Reactive;
+using Microsoft.Reactive.Testing;
+using Xunit;
+
+namespace Tests.Interval
+{
+	public class IntervalObserverTests
+	{
+		[Theory]
+		[InlineData(0, 0, 4)]
+		[InlineData(0, 0, 10)]
+		[InlineData(0, 2, 10)]
+		[InlineData(2, 2, 20)]
+		[InlineData(2, 5, 20)]
+		public void return_sequence_of_notifications__when__subscribe_to_interval_distributor(int created, int subscribed, int disposed)
+		{
+			//arrange
+			_fixture.SetSimulationData(created, subscribed, disposed);
+
+			//act
+			var result = act();
+
+			//assert__simulation_notification
+			_fixture.assert__simulation_notification(result);
+		}
+
+		private IList<Recorded<Notification<long>>> act()
+		{
+			return _fixture.act();
+		}
+
+		public IntervalObserverTests()
+		{
+			_fixture = new IntervalObserverTestsFixture();
+		}
+
+		private IntervalObserverTestsFixture _fixture;
+	}
+}
